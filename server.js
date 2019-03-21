b6f9123066438f28ad0d510ba35ee8a89b81dcee
Vanessa Wang <sanguliupo@gmail.com>
2019-03-21 15:20:31 -0700

    initial commit to github

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    database: 'database'
+  }
+});
+
+db.select('*').from('profiles').then(data =>{
+	console.log(data);
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res)=>{
+
+	const {description, name} = req.body;
+	db('profiles')
+	  .returning('*')
+	  .insert({
+		name:name,
+		description: description,
+	}).then(profile=>{
+		res.json(profile);
+	})
+	.catch(err => res.status(400).json("unable to create profile"))	
+})
+
+
+app.post("/register", (req, res)=>{
+	const {description, name} = req.body;
+	db('profiles')
+	  .returning('*')
+	  .insert({
+		name:name,
+		description: description,
+	})
+	  .then(response =>{
+		res.json(response);
+	})
+	.catch(err => res.status(400).json("unable to create profile"))	
+})
+
+
+app.listen(3000, ()=>{
+	console.log("app running on port 3000")
+});
+
+
+
+
+/*
+
+/-->this is working
+/signin-->POST  success/fail
+/register -->POST = user
+/profile/:userId -->GET =user
+
+
+*/
