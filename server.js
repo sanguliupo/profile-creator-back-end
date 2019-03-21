b04d059b22a085b93b3c44262ee9f305713bdf26
Vanessa Wang <sanguliupo@gmail.com>
2019-03-21 15:33:14 -0700

    adding process.env.PORT

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ app.post("/register", (req, res)=>{
 })
 
 
-app.listen(3000, ()=>{
-	console.log("app running on port 3000")
+app.listen(process.env.PORT || 3000, ()=>{
+	console.log("app running")
 });
 
 
